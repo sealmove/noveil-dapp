5e68499e6e969fdd8c3042c7019e7dabe1725c64
Stefanos Mandalas <sealmove@proton.me>
2025-04-03 23:21:39 +0200

    export verification key

--- proofs/build.sh
+++ proofs/build.sh
@@ -1,3 +1,4 @@
 mkdir -p ./circuits/build/keys
 circom ./circuits/circuit.circom --r1cs --wasm -o ./circuits/build
 snarkjs groth16 setup ./circuits/build/circuit.r1cs powersOfTau28_hez_final_12.ptau ./circuits/build/keys/circuit_0000.zkey
+snarkjs zkey export verificationkey ./circuits/build/keys/circuit_0000.zkey ./circuits/build/keys/verification_key.json
